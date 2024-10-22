9de73489cf42de493b465bb7ba4ca70039b797ae
mmJDV <mmount@jdvproducts.com>
2024-10-22 10:19:17 -0400

    test meal screen - works

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/meals.dart';
+
+import 'package:meals_app/data/dummy_data.dart';
+
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -23,7 +27,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen(),
+      home: const CategoriesScreen()
     );
   }
 }
