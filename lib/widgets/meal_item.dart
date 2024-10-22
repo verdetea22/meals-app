77040f071e969702b125b4bf25b49b6aec40b695
mmJDV <mmount@jdvproducts.com>
2024-10-22 11:40:09 -0400

    meal item formatting

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -13,46 +13,56 @@ class MealItem extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Card(child: InkWell(
-      onTap: (){},
-      child: Stack(
-        children: [
-          FadeInImage(
-            placeholder: MemoryImage(kTransparentImage), 
-            image: NetworkImage(meal.imageUrl)
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              color: Colors.black54,
-              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 44),
-              child: Column(
-                children: [
-                  Text(
-                    meal.title,
-                    maxLines: 2, 
-                    textAlign: TextAlign.center,
-                    softWrap: true,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const SizedBox(height: 12),
-                  Row(
-                    children: [
-                      
-                    ],
-                  )
-                ],
-              ),
-            ),)
-        ],
+    return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8)
       ),
-    ),);
+      clipBehavior: Clip.hardEdge,
+      elevation: 2,
+      child: InkWell(
+        onTap: (){},
+        child: Stack(
+          children: [
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage), 
+              image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                color: Colors.black54,
+                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 44),
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      maxLines: 2, 
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    const Row(
+                      children: [],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
